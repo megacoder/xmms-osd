0a4174f4ad8c5c5a5dad24d8c5bbdd0b5aaba7ef
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2013-01-12 20:18:57 -0600

    Correctly report an unpause.

--- src/xmms-osd.c
+++ src/xmms-osd.c
@@ -14,6 +14,13 @@
 
 #include <gcc-compat.h>
 
+typedef	enum	states_e	{
+	state_unknown	= 0,
+	state_stopped	= 1,
+	state_paused	= 2,
+	state_playing	= 3
+} states_t;
+
 static	char const	font[] = "-misc-fixed-medium-r-normal--13-120-75-75-c-70-iso8859-1";
 static	unsigned const	debug = 0;
 static	unsigned int const usecs = 750000;	/* 0.75 seconds		 */
@@ -30,6 +37,16 @@ display(
 
 	va_start( ap, fmt );
 	if( vasprintf( &s, fmt, ap ) > 0 )	{
+		char *	eos;
+		/* Trim leading whitespace				 */
+		for( ; *s && isspace( *s ); ++s );
+		/* Trim trailing whitespace				 */
+		eos = s + strlen( s );
+		for(
+			eos = s + strlen( s );
+			(eos > s) && isspace( eos[-1] );
+			*--eos = '\0'
+		);
 		/* Show our work					 */
 		if( debug )	{
 			puts( s );
@@ -40,7 +57,6 @@ display(
 			XOSD_string,
 			s
 		);
-		free( s );
 	}
 	va_end( ap );
 }
@@ -55,6 +71,7 @@ main(
 	gint		session;
 	gint		last_pos;
 	gint		last_vol;
+	states_t	state;
 
 	if( debug )	{
 		printf( "Version %s\n", VERSION );
@@ -62,6 +79,7 @@ main(
 	session = 0;		/* This is probably crap		 */
 	last_pos = -1;
 	last_vol = -1;
+	state = state_unknown;
 	/* Set up the XOSD window					 */
 	xosd_set_align( osd, XOSD_center );
 	xosd_set_pos( osd, XOSD_bottom );
@@ -72,37 +90,60 @@ main(
 	xosd_set_timeout( osd, 18 );
 	xosd_set_shadow_offset( osd, 1 );
 	for( ; ; )	{
-		if(xmms_remote_is_playing( session ) )	{
-			gint		pos;
-			gint		vol;
+		char *		msg;
 
-			pos = xmms_remote_get_playlist_pos( session );
-			vol = xmms_remote_get_main_volume( session );
-			if( pos != last_pos )	{
-				last_pos = pos;
-				char *	title = xmms_remote_get_playlist_title(
-							session,
-							pos
-						);
-				char *		bp;
+		msg = NULL;
+		do	{
+			if( xmms_remote_is_playing( session ) )	{
+				gint		pos;
+				gint		vol;
 
-				last_pos = pos;
-				/* Drop trailing blanks			 */
-				for(
-					bp = title + strlen( title );
-					(bp > title) && bp[-1] && isspace( bp[-1] );
-					*--bp = '\0'
-				);
-				/* Step over leading blanks		 */
-				for( bp = title; *bp && isspace( *bp ); ++bp );
-				display( osd, "%s", bp );
-				free( title );
-				/* Prevent volume overwrite	 */
-				last_vol = vol;
-			} else if( vol != last_vol )	{
-				last_vol = vol;
-				display( osd, "Volume: %02d%%", vol );
+				/* First, check if paused		 */
+				if( xmms_remote_is_paused( session ) )	{
+					/* Is pausing new?		 */
+					if( state != state_paused )	{
+						state = state_paused;
+						msg = strdup( "-[pause]-" );
+					}
+					break;
+				}
+				/* Not paused now, was it?		 */
+				if( state == state_paused )	{
+					/* Force title display		 */
+					last_pos = -1;
+				}
+				state = state_playing;
+				pos = xmms_remote_get_playlist_pos( session );
+				vol = xmms_remote_get_main_volume( session );
+				if( pos != last_pos )	{
+					last_pos = pos;
+					msg = xmms_remote_get_playlist_title(
+								session,
+								pos
+					);
+					last_vol = vol;
+					break;
+				}
+				if( vol != last_vol )	{
+					char	buffer[ BUFSIZ + 1 ];
+
+					last_vol = vol;
+					sprintf( buffer, "-[%02d%%]-", vol );
+					msg = strdup( buffer );
+					break;
+				}
+			} else	{
+				/* Not playing, see if it was last time	 */
+				if( state == state_playing )	{
+					state = state_stopped;
+					msg = strdup( "-[stop]-" );
+					break;
+				}
 			}
+		} while( 0 );
+		if( msg )	{
+			display( osd, "%s", msg );
+			free( msg );
 		}
 		if(usleep( usecs ) == -1 )	{
 			break;
