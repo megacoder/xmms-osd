144aca4464e6f020d2db5fee08924dd18540df28
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-02-24 08:14:32 -0600

    Build RPM packages locally, as a normal non-root user with no prep.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,8 @@ make dist
 rm -rf RPM
 mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
 rpmbuild								\
-	-D"_topdir ${PWD}/RPM"						\
-	-D"_sourcedir ${PWD}"						\
+	-D"_topdir	${PWD}/RPM"					\
+	-D"_sourcedir	${PWD}"						\
+	-D"_specdir	${PWD}"						\
 	-ba								\
 	xmms-osd.spec
