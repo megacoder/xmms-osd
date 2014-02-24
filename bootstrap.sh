96c9c48aeeaa2fc9e8e1671f59f95f3706c5036b
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-02-23 23:09:11 -0600

    Checkpoint

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
-if [ -d .git ]; then
-	git clean -d -f
-else
-	rm -f Makefile Makefile.in src/Makefile src/Makefile.in
-	rm -f xmms-osd.spec configure
-fi
 markdown2 README.md | tee README.html | lynx -dump -stdin >README
 mkdir -p m4
 autoreconf -fvim -I m4
 ./configure
+make
 make dist
 rm -rf RPM
 mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
-set -x
 rpmbuild								\
 	-D"_topdir ${PWD}/RPM"						\
 	-D"_sourcedir ${PWD}"						\
